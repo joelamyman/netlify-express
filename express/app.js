6e5f76a227316911e2f956204a65a5d7eb073cc7
Joe Lamyman <joelamyman@gmail.com>
2020-04-23 10:50:59 +0100

    Second test

--- express/app.js
+++ express/app.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.get('/api/info', (req, res) => {
+app.get('.netlify/functions/app/api/info', (req, res) => {
   res.send({ application: 'sample-app', version: '1' });
 });
-app.post('/api/v1/getback', (req, res) => {
+app.post('.netlify/functions/app/api/v1/getback', (req, res) => {
   res.send({ ...req.body });
 });
 //app.listen(3000, () => console.log(`Listening on: 3000`));
