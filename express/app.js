98e1468006e7a168d6b5c0f1e54fb2914b16938a
Joe Lamyman <joelamyman@gmail.com>
2020-04-23 11:05:31 +0100

    hit and hope

--- express/app.js
+++ express/app.js
@@ -1,6 +1,7 @@
 const serverless = require('serverless-http');
 const express = require('express');
 const app = express();
+const bodyParse = require('body-parser');
 
 const router = express.Router();
 
