7cdcff4a06fe6dbbaa75d3ba0cca0c8f8663d1b8
Joe Lamyman <joelamyman@gmail.com>
2020-05-21 13:48:55 +0100

    trying to uncover the html in the page

--- express/app.js
+++ express/app.js
@@ -27,6 +27,10 @@ router.get('/complete/', (req, res) => {
       if (err) throw err;
       console.log(result);
       client.close();
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify(result)
+      });
     });
   })
 });
