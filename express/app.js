81d67590e0a6e0f408e331e25eb126f5d5ada16b
Joe Lamyman <joelamyman@gmail.com>
2020-05-22 17:16:20 +0100

    Trying to get a build to work again

--- express/app.js
+++ express/app.js
@@ -39,7 +39,7 @@ router.get('/results/share/*', (req, res) => {
 router.get('/results/show/*', (req, res) => {
   // res.set({
   //   'Content-Type': 'text/html'
-  // })
+  // }) oops
   // res.send('<h1>Here is what you made!</h1><p>Text size' + req.query.textSize + '<br> Info amount ' + req.query.infoAmount + '</p><script>window.onload = function(){console.log("loaded")}</script>');
   res.sendFile('/complete/index.html');
   res.end();
