4854bb61bd370e9494747c58a52d78a2cd697f06
Joe Lamyman <joelamyman@gmail.com>
2020-05-22 18:04:22 +0100

    Attempted to fix path

--- express/app.js
+++ express/app.js
@@ -41,7 +41,7 @@ router.get('/results/show/*', (req, res) => {
   //   'Content-Type': 'text/html'
   // }) oops
   // res.send('<h1>Here is what you made!</h1><p>Text size' + req.query.textSize + '<br> Info amount ' + req.query.infoAmount + '</p><script>window.onload = function(){console.log("loaded")}</script>');
-  res.sendFile('index.html');
+  res.sendFile(path.join(__dirname, '../index.html'));
   res.end();
 });
 router.post('/', (req, res) =>  {
